22ad379f81cf1a3d375fe93627422fcfaea0f9f6
purpletemperance08-crypto <purpletemperance08@gmail.com>
2026-03-13 10:46:16 -0600

    Add files via upload
    Practica sencilla para comprender las listas

---
+++ listas_2(ejemplo_3)/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Task
+{
+   public int id { get; set; }
+   public string Description { get; set; }
+   public bool IsCompleted { get; set; }
+}
+
+public class Program
+{
+   public static void Main()
+   {
+      List<Task> backlog = new List<Task>();
+
+      //inserting tasks in specific positions
+      backlog.Insert(0, new Task { id = 1, Description = "Fix critical bug", IsCompleted = false });
+      backlog.Add(new Task { id = 2, Description = "Update documentation", IsCompleted = true });
+      backlog.Add(new Task { id = 3, Description = "Clean code", IsCompleted = false });
+
+      //Search for a specific task by id
+      Task pendingTask = backlog.Find(t => t.id == 3);
+      if (pendingTask != null) pendingTask.IsCompleted = true;
+
+      //order list by Description
+      backlog.Sort((x, y) => x.Description.CompareTo(y.Description));
+
+      System.Console.WriteLine("Task Backlog state (ordered by alphabetical order):");
+      
+      
+   }
+}
